345f8fcd2a552a69e66ec73ba6201fd844916e03
Alexander Monzon <mmonzonp4@miumg.edu.gt>
2024-05-11 20:50:14 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Clase___04_05_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelContracena = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxContrasena = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Name = "label1";
+            // 
+            // labelUsuario
+            // 
+            resources.ApplyResources(this.labelUsuario, "labelUsuario");
+            this.labelUsuario.BackColor = System.Drawing.Color.Gold;
+            this.labelUsuario.Name = "labelUsuario";
+            // 
+            // labelContracena
+            // 
+            resources.ApplyResources(this.labelContracena, "labelContracena");
+            this.labelContracena.BackColor = System.Drawing.Color.Gold;
+            this.labelContracena.Name = "labelContracena";
+            // 
+            // textBoxUsuario
+            // 
+            resources.ApplyResources(this.textBoxUsuario, "textBoxUsuario");
+            this.textBoxUsuario.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            // 
+            // textBoxContrasena
+            // 
+            resources.ApplyResources(this.textBoxContrasena, "textBoxContrasena");
+            this.textBoxContrasena.Name = "textBoxContrasena";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxContrasena);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.labelContracena);
+            this.Controls.Add(this.labelUsuario);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelContracena;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxContrasena;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Formularios/FrmRegistro.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Clase___04_05_2024.Formularios
+{
+    partial class FrmRegistro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxEdad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerFechaIngreso = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrse su Edad";
+            // 
+            // textBoxEdad
+            // 
+            this.textBoxEdad.Location = new System.Drawing.Point(262, 53);
+            this.textBoxEdad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxEdad.Name = "textBoxEdad";
+            this.textBoxEdad.Size = new System.Drawing.Size(148, 26);
+            this.textBoxEdad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(262, 131);
+            this.textBoxNombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(481, 26);
+            this.textBoxNombre.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 229);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha de Ingreso";
+            // 
+            // dateTimePickerFechaIngreso
+            // 
+            this.dateTimePickerFechaIngreso.Location = new System.Drawing.Point(262, 228);
+            this.dateTimePickerFechaIngreso.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dateTimePickerFechaIngreso.Name = "dateTimePickerFechaIngreso";
+            this.dateTimePickerFechaIngreso.Size = new System.Drawing.Size(298, 26);
+            this.dateTimePickerFechaIngreso.TabIndex = 5;
+            // 
+            // FrmRegistro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.dateTimePickerFechaIngreso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxEdad);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmRegistro";
+            this.Text = "FrmRegistro";
+            this.Load += new System.EventHandler(this.FrmRegistro_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxEdad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFechaIngreso;
+    }
+}
